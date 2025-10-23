887178b1d00f82ecbf0412759806f6379b519790
Gianna Dubinski <167164944+giadubinski@users.noreply.github.com>
2025-10-23 01:22:49 +0000

    Commited Changes

--- src/main/scala/boundingBox.scala
+++ src/main/scala/boundingBox.scala
@@ -6,6 +6,29 @@ import Shape.*
 
 object boundingBox:
   def apply(s: Shape): Location = s match
-    case _ => Location(0, 0, Rectangle(0, 0)) // not yet implemented
+    case Rectangle(w, h) =>
+      Location(0, 0, Rectangle(w, h))
+
+    case Ellipse(rx, ry) =>
+      Location(-rx, -ry, Rectangle(2 * rx, 2 * ry))
+
+    case Location(x, y, shape) =>
+      val Location(u, v, Rectangle(w, h)) = apply(shape)
+      Location(u + x, v + y, Rectangle(w, h))
+
+    case Group() =>
+      Location(0, 0, Rectangle(0, 0))
+
+    case Group(shapes @ _*) =>
+      val boxes = shapes.map(apply)
+      val quads = boxes.map:
+        case Location(u, v, Rectangle(w, h)) => (u, v, u + w, v + h)
+
+      val minX = quads.map(_._1).min
+      val minY = quads.map(_._2).min
+      val maxX = quads.map(_._3).max
+      val maxY = quads.map(_._4).max
+
+      Location(minX, minY, Rectangle(maxX - minX, maxY - minY))
 
 end boundingBox
